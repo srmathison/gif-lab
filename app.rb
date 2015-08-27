c9263ac60b6bdfce1a656fa1079652cf879e3db4
Stephanie <stephanierenee.mathison@gmail.com>
2015-08-27 14:22:49 -0400

    decode images sent from frontend, save as files

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require "base64"
 
 get '/' do 
   "hello world"
@@ -6,6 +7,13 @@ end
 
 post '/gif' do
 
-  params.inspect
+  params[:data].each_with_index do |d, i|
+    File.open("test#{i}.png", "wb") do |f|
+      d = d['data:image/png;base64,'.length .. -1]
+      f.write Base64.decode64(d)
+    end
+  end
+
+  "YAY"
 
 end

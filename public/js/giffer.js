ca4d711cd746fc15006f22972b6b0451187c2235
Stephanie <stephanierenee.mathison@gmail.com>
2015-09-09 18:28:15 -0400

    wip

--- public/js/giffer.js
+++ public/js/giffer.js
@@ -1,5 +1,7 @@
+// array to store images
 var myArray = [];
 
+// captures photo on keyup
 $(document).ready(function(){
   $('#myText').on("keyup", function(e){
     myFunction();
@@ -7,6 +9,7 @@ $(document).ready(function(){
   });
 });
 
+// draws photo to canvas
 function myFunction() {
     var userInput = document.getElementById("myText").value;
     var canvas = document.getElementById("myCanvas");
@@ -15,8 +18,7 @@ function myFunction() {
     context.fillStyle = "black";
     context.fillRect(0,0,canvas.width, canvas.height); //GIF can't do transparent so do white
     context.fillStyle = "white";
-    context.fillStyle = "white";
-    // context.fillText(userInput, 50, 100);
+    context.fillText(userInput, 50, 100);
     CanvasTextWrapper(canvas, userInput, {
         verticalAlign: "top",
         allowNewLine: true,
@@ -24,26 +26,35 @@ function myFunction() {
         paddingX: 20,
         paddingY: 20,
         });
+    // converts image to png
     var image = canvas.toDataURL("image/png");
-    // console.log('<img src="'+image+'"/>');
+    // adds image to end of array
     myArray.push(image);
-    console.log(image);
+    console.log(new Date());
     
   };
 
+// clears canvas in between photos
 function myOtherFunction() {
     var canvas = document.getElementById("myCanvas");
     var context = canvas.getContext("2d");
     context.clearRect(0, 0, canvas.width, canvas.height);   
 };
 
+// converts to gif 
 function submit() {
-    $.post( "https://morning-lake-5623.herokuapp.com/gif", {data: myArray}, function( data ) {
-        //debugger
+    // loading while gif is being made
+    $('body').append("<div class='loading'></div>")
+    // sends array to ImageMagick on app.rb
+    $.post( "http://localhost:4567/gif", {data: myArray}, function( data ) {
+        // removes loading
+        $('.loading').remove();
+        // returns gif to user in html
         $( ".result" ).html( "<img src='" + data + "'>" );
         });
 };
 
+// refresh page to make new gif
 function gifRefresh() {
    window.location.reload();
 }
